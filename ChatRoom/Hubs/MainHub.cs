f0579930177126dec8df6632390fd24c294e5761
shayan <shayanmehmandoost37@yahoo.com>
2023-08-02 22:43:24 +0330

    add initial folders to the solution and implementing the mainHub

---
+++ ChatRoom/Hubs/MainHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Serilog;
+
+namespace ChatRoom.Hubs
+{
+    public class MainHub : Hub
+    {
+        public async Task SendMessageAsync()
+        {
+            try
+            {
+                await Clients.All.SendAsync("SendMessage");
+            }
+            catch (Exception ex) { Log.Error(ex, ""); }
+        }
+    }
+}
--- ChatRoom/Startup.cs
+++ ChatRoom/Startup.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatRoom.Hubs;
 
 namespace ChatRoom
 {
@@ -22,12 +24,14 @@ namespace ChatRoom
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,15 +43,18 @@ namespace ChatRoom
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthorization();
+
+
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+
+                endpoints.MapHub<MainHub>("ChatRoom");
+            });            
         }
     }
 }
