9ccafb3f21ccfa21b004c59c4060dcbc72806bf4
shayan <shayanmehmandoost37@yahoo.com>
2023-08-06 22:24:22 +0330

    implementing another layer in API and configure the configuration of other layers

---
+++ ChatRoom.Api/Startup.cs
@@ -0,0 +1,59 @@
+using ChatRoom.Application;
+using ChatRoom.Infrastructure;
+using ChatRoom.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace ChatRoom.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ChatRoom.Api", Version = "v1" });
+            });
+
+            services.ConfigureApplicationServices();
+            services.ConfigureInfrastructureServices(Configuration);
+            services.ConfigurePersistenceServices(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChatRoom.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- ChatRoom.Infrastructure/InfrastructureServices.cs
+++ ChatRoom.Infrastructure/InfrastructureServices.cs
@@ -3,13 +3,6 @@ using ChatRoom.Application.Models.Email;
 using ChatRoom.Infrastructure.Emails;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
 namespace ChatRoom.Infrastructure
 {
     public static class InfrastructureServices
@@ -17,13 +10,12 @@ namespace ChatRoom.Infrastructure
         public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services
              , IConfiguration configuration)
         {
-
-            //services.Configure<EmailSetting>(configuration.GetSection(""));
-            services.AddTransient<IEmail, EmailSender>(); 
+            //services.Configure<EmailSetting>(configuration.GetSection());
+            services.AddTransient<IEmail, EmailSender>();
             return services;
         }
-      
-      
-      
+
+
+
     }
 }
