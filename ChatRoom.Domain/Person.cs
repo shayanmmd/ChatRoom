aa8b8f8d52f5f230f802a58176c1ec5a61f73f00
shayan <shayanmehmandoost37@yahoo.com>
2023-08-08 14:47:43 +0330

    making the first migration to init database

--- ChatRoom.Domain/Person.cs
+++ ChatRoom.Domain/Person.cs
@@ -1,6 +1,7 @@
 ﻿using ChatRoom.Domain.Commons;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace ChatRoom.Domain
     {
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
-        public Guid Guid { get; set; }
+        [Key]public Guid Guid { get; set; }
         public DateTime Modified { get; set; }
         public bool Status { get; set; }
     }
