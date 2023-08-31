b48c8053026dc3aa2700474838698189a336f098
amh512 <141826682+amh512@users.noreply.github.com>
2023-08-31 14:24:30 +0330

    Login-javas

---
+++ script.js
@@ -0,0 +1,9 @@
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission
+      var phone = document.getElementById("phone").value;
+      var username = document.getElementById("username").value;
+      
+      // Perform further actions with the phone and username
+      console.log("Phone: " + phone);
+      console.log("Username: " + username);
+    });
