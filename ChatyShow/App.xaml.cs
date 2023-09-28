bd17537f669e1093424501a4c51adc7572d60479
shayan <shayanmehmandoost37@yahoo.com>
2023-09-28 11:35:38 +0330

    bad ones

---
+++ ChatyShow/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatyShow
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ChatyShow/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatyShow
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
