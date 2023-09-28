db7a5a9e47ba46991ac75d70da4b806067629af5
shayan <shayanmehmandoost37@yahoo.com>
2023-09-28 11:37:11 +0330

    implementing maui proj

---
+++ ChatyShowMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ChatyShowMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
